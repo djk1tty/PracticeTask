c33974d4461589d59b8ec3ef4cc5d97529f9d01a
djk1tty <daniil.kolokoltsev@mail.ru>
2025-05-18 04:57:05 +0300

    add builder to program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
+using FileExchangerAPI.ActionClass;
+using FileExchangerAPI.Interface;
+using FileExchangerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Formats.Asn1;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ConnectDb");
+builder.Services.AddDbContext<FileExchangeDbContext>(options => options.UseSqlServer(connectionString));
 
 // Add services to the container.
-
+builder.Services.AddTransient<IUser, UserClass>();
+builder.Services.AddTransient<IFile, FileClass>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
